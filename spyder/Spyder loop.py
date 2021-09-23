2b5114db7ccc6a9b31fdc6052beea960dd590bb8
Cuddlybear7 <90571557+Cuddlybear7@users.noreply.github.com>
2021-09-23 21:22:48 +0800

    Add files via upload

---
+++ spyder/Spyder loop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 21:17:16 2021
+
+@author: evanquek
+"""
+
+#Print all integers between 1 to 10 in horizontal order
+for i in range (1,11):
+    print(i, end=" ")
